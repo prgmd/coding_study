fba38a17ff085454bb2d2e04326368f4aee058d9
xxj15 <seojini00@naver.com>
2026-01-29 16:23:43 +0900

    좌표 정렬하기 2 (실5)

--- 2주차 🌿/안서진/11651.py
+++ 2주차 🌿/안서진/11651.py
@@ -1,1 +1,13 @@
-# 여기에 코드를 입력해주세요.
+# 좌표 정렬하기 2 (실5)
+
+N = int(input())
+number_list = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    number_list.append((x,y))
+
+number_list.sort(key = lambda x: (x[1], x[0]))
+
+for number in number_list:
+    print(*number)
