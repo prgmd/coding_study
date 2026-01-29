a322d8e90f990b91ffe6ac155f6ef24f1571ae1e
prgmd <neon9008@gmail.com>
2026-01-29 16:18:48 +0900

    장준환 1874, 1966 풀이 업로드

--- 2주차 🌿/장준환/1874.py
+++ 2주차 🌿/장준환/1874.py
@@ -1,1 +1,25 @@
-# 여기에 코드를 입력해주세요.
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+answer = []
+num = 1
+
+for _ in range(n):
+    now = int(input())
+    
+    while num <= now:
+        stack.append(num)
+        answer.append('+')
+        num += 1
+    
+    if stack[-1] == now:
+        stack.pop()
+        answer.append('-')
+    else:
+        print('NO')
+        exit()
+
+for a in answer:
+    print(a)
