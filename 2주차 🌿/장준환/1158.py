60da3b6e27f72de1f6f798ed95a98cebb5ec4000
prgmd <neon9008@gmail.com>
2026-01-29 15:40:04 +0900

    장준환 1158 풀이 업로드

--- 2주차 🌿/장준환/1158.py
+++ 2주차 🌿/장준환/1158.py
@@ -1,1 +1,19 @@
-# 여기에 코드를 입력해주세요.
+n, k = map(int, input().split())
+number = [i for i in range(1, n+1)]
+answer = []
+pos = k-1
+
+for _ in range(n):
+    if pos >= len(number):
+        pos = pos % len(number)
+    answer.append(number.pop(pos))
+    pos += k-1
+
+print('<', end = '')
+for i in range(n):
+    print(answer[i], end = '')
+    if i == n-1:
+        break
+    else:
+        print(', ', end = '')
+print('>')
