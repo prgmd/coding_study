dab7af170a4dbad13b453c03d84f3c2cb23eeef4
seojin <seojini00@naver.com>
2026-02-02 15:48:47 +0900

    0202 문제 (스택 수열)

--- 2주차 🌿/안서진/1874.py
+++ 2주차 🌿/안서진/1874.py
@@ -1,1 +1,25 @@
-# 여기에 코드를 입력해주세요.
+# 스택 수열 (실2)
+
+N = int(input())
+numbers = list(int(input()) for _ in range(N))
+
+stack = []
+ans = []
+cur = 1 
+for num in numbers:
+    while cur <= num:
+        stack.append(cur)
+        ans.append('+')
+        cur += 1
+
+    if stack[-1] == num :
+        ans.append('-')
+        stack.pop()
+    
+    else: 
+        print('NO')
+        break
+
+else:
+    print(*ans, sep = '\n')
+
